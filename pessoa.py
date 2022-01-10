87e0bc212f2abab475c677ace63b7b1327848971
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2022-01-10 11:09:39 -0300

    Removido código desnecessário

--- pessoa.py
+++ pessoa.py
@@ -1,9 +1,3 @@
-from __future__ import print_function
-import sys
-
-if sys.version_info < (3, 0):
-    input = raw_input
-
 class Pessoa(object):
     def __init__(self, nome):
         self.nome = nome
